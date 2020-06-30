05e31ef4a46a8de3969d0b29fbf3377400e7e650
Joshua anderson <Mp4ct@live.com>
2020-06-29 23:20:01 -0500

    Push to Github

---
+++ src/main/java/com/springboot/web/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.springboot.web.controller;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.springboot.web.service.LoginService;
+
+@Controller
+@SessionAttributes("name")
+public class LoginController {
+	
+	@Autowired
+	LoginService service;
+	//@GetMapping("/login")
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model){
+		return "login";
+	}
+	@Autowired
+	private MessageSource messageSource;
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String showWelcomePage(Locale locale, ModelMap model, @RequestParam String name, @RequestParam String password){
+		
+		boolean isValidUser = service.validateUser(name, password);
+		
+		if (!isValidUser) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "login";
+		}
+		model.put("clickhere",messageSource.getMessage("clickhere.message",null, locale));
+		model.put("statement",messageSource.getMessage("statement.message",null, locale));
+		model.put("welcome",messageSource.getMessage("welcome.message",null, locale));
+		model.put("name", name);
+		model.put("password", password);
+		
+		return "welcome";
+	}
+
+}
